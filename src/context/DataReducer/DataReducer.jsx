4577c99669094e7834c85b52766b4152631ed32b
mayuresh-tech <mmayuresh12@gmail.com>
2022-04-17 23:56:06 +0530

    feat: added basic filters

--- src/context/DataReducer/DataReducer.jsx
+++ src/context/DataReducer/DataReducer.jsx
@@ -10,10 +10,33 @@ export const DataReducer = (state, action) => {
     case "LOAD_CART":
       localStorage.setItem("cart", JSON.stringify(action.payload));
       return { ...state, cart: action.payload };
-    case "CLEAR":
-      return {};
     case "LOGOUT":
       return { ...state, wishlist: [], cart: [] };
+    case "MEN":
+      return { ...state, men: !state.men };
+    case "WOMEN":
+      return { ...state, women: !state.women };
+    case "ELECTRONICS":
+      return { ...state, electronics: !state.electronics };
+    case "DECOR":
+      return { ...state, decor: !state.decor };
+    case "RATING":
+      return { ...state, rating: action.payload };
+    case "PRICE":
+      return { ...state, price: action.payload };
+    case "SORT":
+      return { ...state, sortBy: action.payload };
+    case "CLEAR":
+      return {
+        ...state,
+        price: 500,
+        rating: 0,
+        sortBy: null,
+        men: false,
+        women: false,
+        electronics: false,
+        decor: false,
+      };
     default:
       return state;
   }
